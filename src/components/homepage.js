e001f82817990206132bc71bf4f5d7bc2bc7e50b
mvillaronga-medkoder <mvillaronga@medkoder.com>
2015-08-31 07:37:20 -0500

    Created and server sync

---
+++ src/components/homepage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1>Plural sight administration</h1>
+        <p>React, react router, and flux for ultra responsive web apps.</p>
+      </div>
+    );
+  }
+});
+
+
+module.exports = Home;
